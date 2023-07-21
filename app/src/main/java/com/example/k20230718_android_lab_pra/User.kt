c92f0aee3d59c94be56dff6e18c187f47cd26b62
조경제 <jskytorod@gmail.com>
2023-07-21 09:15:12 +0900

    first commit

---
+++ app/src/main/java/com/example/k20230718_android_lab_pra/User.kt
@@ -0,0 +1,24 @@
+package com.example.k20230718_android_lab_pra
+
+import java.util.Scanner
+
+
+class Test {
+
+
+}
+
+fun main() {
+    var sc =  Scanner(System.`in`);
+    print("이름:")
+    sc.next();
+    print("ID:")
+    sc.next();
+    print("PW:")
+    sc.next();
+    print("email:")
+    sc.next();
+    print("phone:")
+    sc.next();
+
+}
